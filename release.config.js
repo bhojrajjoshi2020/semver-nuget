5862dc74c2f7b413e597a6c124fdbe80fd134f1e
bhojrajjoshi2020 <60560151+bhojrajjoshi2020@users.noreply.github.com>
2020-11-23 13:19:22 +0530

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,43 +1,41 @@
-module.exports = {
-    verifyConditions: [
-        // Verifies a NUGET_TOKEN environment variable has been provided
-        () => {
-            if (!process.env.NUGET_TOKEN) {
-                throw new SemanticReleaseErrorc(
-                    'The environment variable NUGET_TOKEN is required.',
-                    'ENOAPMTOKEN',
-                )
-            }
-        },
-        // Verifies the conditions for the plugins used below
-        // For example verifying a GITHUB_TOKEN environment variable has been provided
-        '@semantic-release/changelog',
-        '@semantic-release/git',
-        '@semantic-release/github',
-    ],
-    publish: [
-        // https://github.com/semantic-release/changelog
-        // Set of semantic-release plugins for creating or updating a changelog file.
-        '@semantic-release/changelog',
-
-        // https://github.com/semantic-release/git
-        // Git plugin is need so the changelog file will be committed to the Git repository and available on subsequent builds in order to be updated.
-        '@semantic-release/git',
-
-        // https://github.com/semantic-release/git
-        // Exec plugin uses to call dotnet nuget push to push the packages from 
-        // the artifacts folder to NuGet
-        {
-            path: '@semantic-release/exec',
-            cmd: `dotnet nuget push .\\artifacts\\*.nupkg -k ${process.env.NUGET_TOKEN} -s https://api.nuget.org/v3/index.json`,
-        },
-        
-        // https://github.com/semantic-release/github
-        // Set of Semantic-release plugins for publishing a GitHub release.
-        // Includes the packages from the artifacts folder as assets
-        {
-            path: '@semantic-release/github',
-            assets: 'artifacts/**/*.nupkg',
-        },
-    ],
+{
+  "name": "@bhojrajjoshi2020/semvertest",
+  "version": "0.0.0",
+  "description": "Sample code for semvertest",
+  "main": "constants.js",
+  "dependencies": {
+    "authorizenet": "^1.0.7",
+    "chai": "^3.5.0",
+    "eslint": "^2.12.0"
+  },
+  "devDependencies": {},
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/bhojrajjoshi2020/semvertest.git"
+  },
+  "keywords": [
+    "AuthorizeNet",
+    "sample",
+    "codes"
+  ],
+  "author": "",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/bhojrajjoshi2020/semvertest/issues"
+  },
+  "homepage": "https://github.com/bhojrajjoshi2020/semvertest#readme",
+  "publishConfig": {
+    "registry": "https://npm.pkg.github.com/"
+  },
+  "plugins": [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    ["@semantic-release/exec", {
+      #"verifyConditionsCmd": "./verify.sh",
+      "publishCmd": "./publish.sh ${nextRelease.version} ${options.branch} ${commits.length} ${Date.now()}"
+    }],
+  ]
 }
