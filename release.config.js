295f2276c0a27f4163467d1b6a603ef66d686552
bhojrajjoshi2020 <60560151+bhojrajjoshi2020@users.noreply.github.com>
2020-11-23 12:48:53 +0530

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,45 +1,43 @@
 module.exports = {
-  verifyConditions: [
-    () => {
-      if (!process.env.NUGET_TOKEN) {
-        throw new SemanticReleaseError(
-          'The environment variable NUGET_TOKEN is required.',
-          'ENOAPMTOKEN',
-        )
-      }
-    },
-    '@semantic-release/changelog',
-    '@semantic-release/git',
-    '@semantic-release/github',
-  ],
-  prepare: ['@semantic-release/changelog', '@semantic-release/git'],
-  publish: [
-    {
-      path: '@semantic-release/exec',
-      cmd: `dotnet nuget push .\\artifacts\\*.nupkg -k ${
-        process.env.NUGET_TOKEN
-      } -s https://api.nuget.org/v3/index.json`,
-    },
-    {
-      path: '@semantic-release/github',
-      assets: 'artifacts/**/*.nupkg',
-    },
-  ],
-}
+    verifyConditions: [
+        // Verifies a NUGET_TOKEN environment variable has been provided
+        () => {
+            if (!process.env.NUGET_TOKEN) {
+                throw new SemanticReleaseErrorc(
+                    'The environment variable NUGET_TOKEN is required.',
+                    'ENOAPMTOKEN',
+                )
+            }
+        },
+        // Verifies the conditions for the plugins used below
+        // For example verifying a GITHUB_TOKEN environment variable has been provided
+        '@semantic-release/changelog',
+        '@semantic-release/git',
+        '@semantic-release/github',
+    ],
+    publish: [
+        // https://github.com/semantic-release/changelog
+        // Set of semantic-release plugins for creating or updating a changelog file.
+        '@semantic-release/changelog',
 
-/*
-HACK: We should really be importing the semantic-release error package as below:
-const SemanticReleaseError = require('@semantic-release/error');
-and using that, however the import was failing when running semantic-release using npx
-even when adding the @semantic-release/error package to npx.
-Resorting to copying the source into this file instead
-*/
-class SemanticReleaseError extends Error {
-  constructor(message, code) {
-    super(message)
-    Error.captureStackTrace(this, this.constructor)
-    this.name = 'SemanticReleaseError'
-    this.code = code
-    this.semanticRelease = true
-  }
+        // https://github.com/semantic-release/git
+        // Git plugin is need so the changelog file will be committed to the Git repository and available on subsequent builds in order to be updated.
+        '@semantic-release/git',
+
+        // https://github.com/semantic-release/git
+        // Exec plugin uses to call dotnet nuget push to push the packages from 
+        // the artifacts folder to NuGet
+        {
+            path: '@semantic-release/exec',
+            cmd: `dotnet nuget push .\\artifacts\\*.nupkg -k ${process.env.NUGET_TOKEN} -s https://api.nuget.org/v3/index.json`,
+        },
+        
+        // https://github.com/semantic-release/github
+        // Set of Semantic-release plugins for publishing a GitHub release.
+        // Includes the packages from the artifacts folder as assets
+        {
+            path: '@semantic-release/github',
+            assets: 'artifacts/**/*.nupkg',
+        },
+    ],
 }
